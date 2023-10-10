bebc2195c18be6ad5a67f1f5781d44c35f0e9a8f
VITOR SANTANA DE JESUS <vitor.alunos@cfp127aluno.local>
2023-10-10 16:56:21 -0300

    Heraca-Zoo

---
+++ src/br/senai/sp/jandira/model/Moto.java
@@ -0,0 +1,33 @@
+package br.senai.sp.jandira.model;
+
+import java.util.Scanner;
+
+public class Moto extends Veiculos {
+
+    boolean bau, antena;
+    Scanner scanner = new Scanner(System.in);
+
+    public Moto(String marca){
+        super(marca);
+    }
+
+    public void cadastrarMoto(){
+
+        System.out.println("/------- Cadastrar Moto -------/");
+        System.out.println("Informe a marca:                ");
+        super.setMarca(scanner.nextLine());
+        System.out.println("Informe o modelo:               ");
+        super.setModelo(scanner.nextLine());
+        System.out.println("Informe a placa:                ");
+        super.setPlaca(scanner.nextLine());
+        System.out.println("Informe o ano da Moto:          ");
+        super.setAno(scanner.nextInt());
+        scanner.nextLine();
+        System.out.println("Informe se a moto tem bau: [true - false] ");
+        bau = scanner.nextBoolean();
+        System.out.println("Informe se a moto tem antena: [true - false]   ");
+        antena = scanner.nextBoolean();
+        System.out.println("/-------------------------------/");
+
+    }
+}
